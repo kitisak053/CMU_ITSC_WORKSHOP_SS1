fe14707c9570637b07712bb07248ce666c6072e8
CHEEWIN BUAPAN <cheewin.b@cmu.ac.th>
2021-01-15 14:50:46 +0700

    update

--- workshop_docker_compose/Controllers/ValuesController.cs
+++ workshop_docker_compose/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace workshop_docker_compose.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "best1", "best2" };
         }
 
         // GET api/values/5
